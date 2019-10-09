6f234aff08eb3f670ce1c1bd742543e25633620f
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2019-10-09 23:15:32 +0200

    Build docs

---
+++ LICENSE.md
@@ -0,0 +1,1 @@
+[https://creativecommons.org/publicdomain/zero/1.0/](https://creativecommons.org/publicdomain/zero/1.0/)
---
+++ scripts/build-docs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Ref: https://github.com/frenck/awesome-home-assistant/blob/master/scripts/build.sh
+
+set -o errexit  # Exit script when a command exits with non-zero status
+set -o errtrace # Exit on error inside any functions or sub-shells
+set -o nounset  # Exit script on use of an undefined variable
+set -o pipefail # Return exit status of the last command in the pipe that failed
+
+# Switch to the root of the repository.
+cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
+
+# Install requirements
+pip install -r requirements.txt
+
+# Get the pages into the docs folder
+ln -sf ../README.md docs/index.md
+ln -sf ../LICENSE.md docs/license.md
+ln -sf ../CONTRIBUTING.md docs/contributing.md
+
+# Build!
+mkdocs build
